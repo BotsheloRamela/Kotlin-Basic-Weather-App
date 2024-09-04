5cd1d14aebea778b721efcc2b9653f2659030a1d
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-09-04 23:05:39 +0200

    CHORE(ui): Add weather icons

---
+++ app/src/main/java/com/botsheloramela/basicweatherapp/ui/views/HomeView.kt
@@ -0,0 +1,11 @@
+package com.botsheloramela.basicweatherapp.ui.views
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeView() {
+    Column {
+
+    }
+}
